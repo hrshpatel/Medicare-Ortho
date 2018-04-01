3f911e0f030a65809a98caf153959a9f0335a0a9
hrshpatel <hrshpatel06@gmail.com>
2018-04-01 10:54:59 +0530

    First commit

---
+++ app/src/main/java/com/ortho/medicare/medicareortho/SplashScreenActivity.java
@@ -0,0 +1,13 @@
+package com.ortho.medicare.medicareortho;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+    }
+}
